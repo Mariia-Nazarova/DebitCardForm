efebec48b647b60d289b71b99c9fac8c98b46e27
Mariia Nazarova <Djungary@mail.ru>
2023-04-24 22:34:19 +0300

    commit

--- src/test/java/DebitCardTestPositive.java
+++ src/test/java/DebitCardTestPositive.java
@@ -24,6 +24,7 @@ public class DebitCardTestPositive {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
 
